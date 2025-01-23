9d4496e0f80b78d8b8c8256f19a8fbcabfc3d23d
TomSmail <thomas.c.smail@gmail.com>
2025-01-23 15:21:50 +0000

    fix: add try statement

--- square.py
+++ square.py
@@ -1,5 +1,5 @@
 import brickpi3 #import BrickPi.py file to use BrickPi operations
-
+y
 import curses   # import curses for text processing
 
 # set up curses interface
@@ -12,19 +12,28 @@ motorR = BP.PORT_B # right motor
 motorL = BP.PORT_C # left motor
 speed = 200 # range is -255 to 255, make lower if bot it too fast
 
-degrees = [90 * i for i in range(25)]
+def stop():
+        BP.set_motor_power(motorR, 0)
+        BP.set_motor_power(motorL, 0)
 
-for degree in degrees: 
-    BP.set_motor_position(motorR, degree)
-    BP.set_motor_position(motorL, -degree)
 
-    print("degree: ", degree)
+if __name__ == "__main__":
+    degrees = [90 * i for i in range(25)]
+    try:
+        for degree in degrees: 
+            BP.set_motor_position(motorR, degree)
+            BP.set_motor_position(motorL, -degree)
 
-    input("Press Enter to continue...")
-    inp = stdscr.getkey() #Take input from the terminal
+            print("degree: ", degree)
 
-    if inp == 'q':
-        break
-    else:
-        pass
+            input("Press Enter to continue...")
+            inp = stdscr.getkey() #Take input from the terminal
 
+            if inp == 'q':
+                break
+            else:
+                pass
+
+    except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+        stop()
+        print("Keyboard Interrupt")
