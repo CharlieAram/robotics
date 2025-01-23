0cb4395472bc6f7fab3c30dd4c574b60bcc6dc67
Alex <an2022@ic.ac.uk>
2025-01-23 22:03:31 +0000

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+
+import time
+import brickpi3
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+motorR = BP.PORT_B # right motor
+motorL = BP.PORT_C # left motor
+
+ROTS_FWD = 4.244
+ROTS_TURN = 1.142
+
+BP.set_motor_position_relative(motorL, 360 * ROTS_TURN)
+BP.set_motor_position_relative(motorR, -360 * ROTS_TURN)
