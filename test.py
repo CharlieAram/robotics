696138ea810c500431a77783b31e1ae1d050cf5d
CharlieAram <charlie21aram@gmail.com>
2025-01-28 16:27:21 +0000

    Adjust test.py

--- test.py
+++ test.py
@@ -7,17 +7,17 @@ motorR = BP.PORT_B # right motor
 motorL = BP.PORT_C # left motor
 
 ROTS_FWD = 4.244
-ROTS_TURN = 1.142
+ROTS_TURN = 1.142 - 0.01
 
 for _ in range(4):
     BP.set_motor_position_relative(motorL, 360 * ROTS_FWD)
     BP.set_motor_position_relative(motorR, 360 * ROTS_FWD)
 
-    time.sleep(2)
+    time.sleep(0.5)
     print("Finished fwd")
 
     BP.set_motor_position_relative(motorL, 360 * ROTS_TURN)
     BP.set_motor_position_relative(motorR, -360 * ROTS_TURN)
 
-    time.sleep(1)
+    time.sleep(0.5)
     print("Finished rotating")
