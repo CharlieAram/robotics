2cfbb026251452ab1aee4717d571e8a353eff70f
CharlieAram <charlie21aram@gmail.com>
2025-01-28 16:51:12 +0000

    test.py - reset motor encoders:

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ motorL = BP.PORT_C # left motor
 ROTS_FWD = 4.244 + diff
 ROTS_TURN = 1.142 - 0.015
 
+BP.offset_motor_encoder(motorL, BP.get_motor_encoder(motorL))
+BP.offset_motor_encoder(motorR, BP.get_motor_encoder(motorR))
 for _ in range(4):
     BP.set_motor_position_relative(motorL, 360 * ROTS_FWD)
     BP.set_motor_position_relative(motorR, 360 * ROTS_FWD)
