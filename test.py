249a9255c7b05dccb4ee8250397b254de3b83d3e
CharlieAram <charlie21aram@gmail.com>
2025-01-29 16:11:22 +0000

    Test(test.py): switched L and R and added try except

--- test.py
+++ test.py
@@ -5,26 +5,32 @@ import sys
 diff = float(sys.argv[1]) if len(sys.argv)>1 else 0
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
-motorR = BP.PORT_B # right motor
-motorL = BP.PORT_C # left motor
+motorL = BP.PORT_B # right motor
+motorR= BP.PORT_C # left motor
 
 ROTS_FWD = 4.244 + diff
 ROTS_TURN = 1.142 - 0.015
-BP.offset_motor_encoder(motorL, BP.get_motor_encoder(motorL))
-BP.offset_motor_encoder(motorR, BP.get_motor_encoder(motorR))
 
-for _ in range(4):
-    BP.set_motor_limits(motorL, power=20)
-    BP.set_motor_limits(motorR, power=20)
-    BP.set_motor_position_relative(motorL, 360 * ROTS_FWD)
-    BP.set_motor_position_relative(motorR, 360 * ROTS_FWD)
+try:
+    BP.offset_motor_encoder(motorL, BP.get_motor_encoder(motorL))
+    BP.offset_motor_encoder(motorR, BP.get_motor_encoder(motorR))
 
-    time.sleep(10)
-    print("Finished fwd")
-    BP.set_motor_limits(motorL, power=20)
-    BP.set_motor_limits(motorR, power=20)
-    BP.set_motor_position_relative(motorL, 360 * ROTS_TURN)
-    BP.set_motor_position_relative(motorR, -360 * ROTS_TURN)
+    for _ in range(4):
+        BP.set_motor_limits(motorL, power=20)
+        BP.set_motor_limits(motorR, power=20)
+        BP.set_motor_position_relative(motorL, 360 * ROTS_FWD)
+        BP.set_motor_position_relative(motorR, 360 * ROTS_FWD)
 
-    time.sleep(4.5)
-    print("Finished rotating")
+        time.sleep(10)
+        print("Finished fwd")
+        BP.set_motor_limits(motorL, power=20)
+        BP.set_motor_limits(motorR, power=20)
+        BP.set_motor_position_relative(motorL, 360 * ROTS_TURN)
+        BP.set_motor_position_relative(motorR, -360 * ROTS_TURN)
+
+        time.sleep(4.5)
+        print("Finished rotating")
+except Exception as e:
+    print("Error: ", e)
+
+BP.reset_all()
