2bb35bddb3bd5ceacd26a52c701fe0c7c886b949
CharlieAram <charlie21aram@gmail.com>
2025-01-29 15:59:29 +0000

    Fix(test.py): motor limits instead of set power

--- test.py
+++ test.py
@@ -14,17 +14,17 @@ BP.offset_motor_encoder(motorL, BP.get_motor_encoder(motorL))
 BP.offset_motor_encoder(motorR, BP.get_motor_encoder(motorR))
 
 for _ in range(4):
-    BP.set_motor_power(motorL, 20)
-    BP.set_motor_power(motorR, 20)
+    BP.set_motor_limits(motorL, power=20)
+    BP.set_motor_limits(motorR, power=20)
     BP.set_motor_position_relative(motorL, 360 * ROTS_FWD)
     BP.set_motor_position_relative(motorR, 360 * ROTS_FWD)
 
-    time.sleep(2)
+    time.sleep(8)
     print("Finished fwd")
-    BP.set_motor_power(motorL, 20)
-    BP.set_motor_power(motorR, 20)
+    BP.set_motor_limits(motorL, power=20)
+    BP.set_motor_limits(motorR, power=20)
     BP.set_motor_position_relative(motorL, 360 * ROTS_TURN)
     BP.set_motor_position_relative(motorR, -360 * ROTS_TURN)
 
-    time.sleep(0.5)
+    time.sleep(2)
     print("Finished rotating")
