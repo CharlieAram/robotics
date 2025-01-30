439e9106ace359bb5b4353e0b58d4b19107df5ae
CharlieAram <charlie21aram@gmail.com>
2025-01-30 13:30:37 +0000

    Tune(test.py): tuning

--- test.py
+++ test.py
@@ -9,10 +9,12 @@ motorR = BP.PORT_B # right motor
 motorL= BP.PORT_C # left motor
 ROTS_FWD = 4.244 * (38/42.5) * 1.12
 ROTS = [ROTS_FWD, ROTS_FWD * 40/42.5] * 2
-ROTS[2] *= 0.93
+ROTS[2] *= 0.97
 ROTS_TURN = 1.142 - 0.015
 ROTST = [ROTS_TURN] * 4
-ROTST[2] *= 0.93
+ROTST[0] *= 0.96
+ROTST[1] *= 0.96
+ROTST[2] *= 0.97
 
 try:
 
