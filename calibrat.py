0cebd1fd32d65215fb4b83d4d4c1af0e7be2bf80
CharlieAram <charlie21aram@gmail.com>
2025-02-19 16:15:16 +0000

    calibrat

--- calibrat.py
+++ calibrat.py
@@ -3,8 +3,8 @@ from probalistic_motion import Robot
 from math import pi as PI
 
 robot = Robot(100, 0, False)
-print(robot.getMeanPos())
 print(robot.driver.read_sensor())
+print("Hello")
 robot.move_forward(10)
-print(robot.getMeanPos())
+print("goodbye")
 print(robot.driver.read_sensor())
