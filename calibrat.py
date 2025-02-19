0cad99fbe1427e43221212aa34489e75ffd6d102
CharlieAram <charlie21aram@gmail.com>
2025-02-19 16:17:45 +0000

    brat

--- calibrat.py
+++ calibrat.py
@@ -1,10 +1,10 @@
 from probalistic_motion import Robot
-
+from mcl import NormRobot
 from math import pi as PI
 
-robot = Robot(100, 0, False)
-print(robot.driver.read_sensor())
+robot = NormRobot(100, 0, False)
+print(robot.sensor_reading())
 print("Hello")
 robot.move_forward(10)
 print("goodbye")
-print(robot.driver.read_sensor())
+print(robot.sensor_reading())
