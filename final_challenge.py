33e6c837068be86581573bd045cebb85bd32191e
CharlieAram <charlie21aram@gmail.com>
2025-03-04 16:59:26 +0000

    move forward

--- final_challenge.py
+++ final_challenge.py
@@ -1,14 +1,19 @@
 import motor_driver
 import brickpi3
 import time
+import sys
+
+power = sys.argv[1]
 
 if __name__ == "__main__":
     motorR = brickpi3.BrickPi3.PORT_B  # right motor
     motorL = brickpi3.BrickPi3.PORT_C  # left motor
     driver = motor_driver.MotorDriver(motorL=motorL, motorR=motorR)
     driver.flipR = True
-    for i in range(0, 255):
-        print(i)
-        driver.write_left(i)
-        driver.write_right(i)
-        time.sleep(0.2)
+    driver.write_left(power)
+    driver.write_right(power)
+    # for i in range(0, 255):
+    #     print(i)
+    #     driver.write_left(i)
+    #     driver.write_right(i)
+    #     time.sleep(0.2)
