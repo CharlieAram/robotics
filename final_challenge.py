db5aa380e8720d0a97e33c4eba5cce2851192761
CharlieAram <charlie21aram@gmail.com>
2025-03-04 16:53:58 +0000

    thing

--- final_challenge.py
+++ final_challenge.py
@@ -1,9 +1,13 @@
 import motor_driver
 import brickpi3
-
+import time
 
 if __name__ == "__main__":
     motorR = brickpi3.BrickPi3.PORT_B  # right motor
     motorL = brickpi3.BrickPi3.PORT_C  # left motor
     driver = motor_driver.MotorDriver(motorL=motorL, motorR=motorR)
-    driver.lane_change()
+    for i in range(0, 255):
+        print(i)
+        driver.write_left(i)
+        driver.write_right(i)
+        time.sleep(0.2)
