bfd828c1eb8920a3423bf271bdd89317bfc166d0
CharlieAram <charlie21aram@gmail.com>
2025-03-04 16:48:42 +0000

    fix

--- final_challenge.py
+++ final_challenge.py
@@ -1,7 +1,9 @@
 import motor_driver
-
+import brickpi3
 
 
 if __name__ == "__main__":
-    driver = motor_driver.MotorDriver()
+    motorR = brickpi3.BrickPi3.PORT_B  # right motor
+    motorL = brickpi3.BrickPi3.PORT_C  # left motor
+    driver = motor_driver.MotorDriver(motorL=motorL, motorR=motorR)
     driver.lane_change()
