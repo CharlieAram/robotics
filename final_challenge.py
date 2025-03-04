1f96ccbb41c454e49c7019cfb1a3d3345d96fd8f
CharlieAram <charlie21aram@gmail.com>
2025-03-04 17:04:44 +0000

    FIX

--- final_challenge.py
+++ final_challenge.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     motorL = brickpi3.BrickPi3.PORT_C  # left motor
     driver = motor_driver.MotorDriver(motorL=motorL, motorR=motorR)
     driver.flipR = True
+    print(POWER)
     driver.write_left(POWER)
     driver.write_right(POWER)
     # for i in range(0, 255):
