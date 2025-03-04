0987677f85a3eaf6123c0daecc484a91d6380732
TomSmail <thomas.c.smail@gmail.com>
2025-03-04 16:39:09 +0000

    sanity

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -100,32 +100,55 @@ if __name__ == "__main__":
         # Convert image to HSV color space
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Define range for red color in HSV
-        lower_red = np.array([0, 120, 70])
-        upper_red = np.array([10, 255, 255])
-        mask1 = cv2.inRange(hsv, lower_red, upper_red)
+        img = picam2.capture_array()
 
-        lower_red = np.array([170, 120, 70])
-        upper_red = np.array([180, 255, 255])
-        mask2 = cv2.inRange(hsv, lower_red, upper_red)
+    # Convert to HSV colour space
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Combine masks
-        mask = mask1 + mask2
+    # Apply colour thresholding: for red this is done in two steps
+    # lower mask (0-10)
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    mask0 = cv2.inRange(hsv, lower_red, upper_red)
+    # upper mask (170-180)
+    lower_red = np.array([170, 50, 50])
+    upper_red = np.array([180, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+    # join my masks
+    mask = mask0 + mask1
+    # This is a thresholded version of the image which you can display if
+    # you want to check what the colour thresholding does
+    result = cv2.bitwise_and(img, img, mask=mask)
 
-        # Find contours
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Calculate connected components: colour thresholded "blob" regions
+    output = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32F)
+    (numLabels, labels, stats, centroids) = output
 
-        # Draw circles around detected red objects
-        for contour in contours:
-            approx = cv2.approxPolyDP(
-                contour, 0.02 * cv2.arcLength(contour, True), True
-            )
-            area = cv2.contourArea(contour)
-            if len(approx) > 8 and area > 30:
-                (x, y), radius = cv2.minEnclosingCircle(contour)
-                center = (int(x), int(y))
-                radius = int(radius)
-                img = cv2.circle(img, center, radius, (0, 255, 0), 2)
+    # Find the properties of the detected blobs
+    for i in range(0, numLabels):
+        # i=0 is the background region so ignore it
+        if i != 0:
+            # Extract the connected component statistics and centroid
+            # Here you can get the limits of the blob if you need them
+            x = stats[i, cv2.CC_STAT_LEFT]
+            y = stats[i, cv2.CC_STAT_TOP]
+            w = stats[i, cv2.CC_STAT_WIDTH]
+            h = stats[i, cv2.CC_STAT_HEIGHT]
+            area = stats[i, cv2.CC_STAT_AREA]
+            (cu, cv) = centroids[i]
+            # Print out the properties of blobs above a certain size
+            if area > 150:
+                print("Component", i, "area", area, "Centroid", cu, cv)
+                cuint = int(cu)
+                cvint = int(cv)
+                # Draw a little circle to show each detected blob
+                img = cv2.circle(img, (cuint, cvint), 5, white, 3)
+                # Also print its coordinates on the image!
+                pstring = "(" + str(cuint) + "," + str(cvint) + ")"
+                img = cv2.putText(
+                    img, pstring, (cuint + 8, cvint), font, 0.5, white, 1, cv2.LINE_AA
+                )
 
         # Draw grid on image
         img = drawGridOnImage(img)
